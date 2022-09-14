514b2aedd74ec7c66834f912800ae6d82454516f
olaotan92 <elijahshawntel@gmail.com>
2022-09-14 11:04:30 -0700

    main c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - testing a function that prints P or N-vity of an integer
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
