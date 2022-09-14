0907015c76f1b22f3c015110dec7e9c3ff8bf8a0
olaotan92 <elijahshawntel@gmail.com>
2022-09-13 19:27:27 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_sign - returns 0 letter not lowercase, 1 letter lowercase
+*
+* @n: number to check
+* Return: Always 0 or 1
+*/
+int print_sign(int n)
+{
+int test;
+if (n > 0)
+{
+test = 1;
+_putchar('+');
+}
+else if (n == 0)
+{
+test = 0;
+_putchar('0');
+}
+else
+{
+test = -1;
+_putchar('-');
+}
+return (test);
+}
