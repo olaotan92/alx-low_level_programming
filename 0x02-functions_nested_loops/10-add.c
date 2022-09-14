78e029e67517b9dc1a421d94dfcb50c30626cce2
olaotan92 <elijahshawntel@gmail.com>
2022-09-13 21:46:09 -0700

    betty correct

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 * add - adds two integers and returns the result
-@a: int to be added to b
-@b: int to be added to a
+* @a: int to be added to b
+* @b: int to be added to a
 * Return: sum of a and b
 */
 int add(int a, int b)
