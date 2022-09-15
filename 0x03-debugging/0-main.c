cb5b9c4d06bacb91f7ce1701772cf4afd9d0f676
olaotan92 <elijahshawntel@gmail.com>
2022-09-15 07:19:33 -0700

    correct

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
+ 
 /**
-* main - testing a function that prints P or N-vity of an integer
-*
-* Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+ 
 int main(void)
 {
 int i;
+ 
 i = 0;
 positive_or_negative(i);
+ 
 return (0);
 }
