a3f0b53054c0726d2a3ab1ff51158fe72c1aaf85
olaotan92 <elijahshawntel@gmail.com>
2022-09-19 12:31:47 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* rev_string - reverses a given string
+* @s: parameter
+*/
+void rev_string(char *s)
+{
+int i, size, half;
+char first, last;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+size = i - 1;
+half = size / 2;
+while (half >= 0)
+{
+first = s[size - half];
+last = s[half];
+s[half] = first;
+s[size - half] = last;
+half--;
+}
+}
