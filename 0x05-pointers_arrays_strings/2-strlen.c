4bd1edd901b483d1de6f93ec5d5e53e8b420ee71
olaotan92 <elijahshawntel@gmail.com>
2022-09-19 07:20:02 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - string length
+* @s: parameter
+* Return: int
+*/
+int _strlen(char *s)
+{
+int count = 0;
+while (s[count] != '\0')
+{
+count++;
+}
+return (count);
+}
