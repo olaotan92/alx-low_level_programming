9a112051d8eb880b5383b8bc2b92f896d555cbc1
olaotan92 <elijahshawntel@gmail.com>
2022-10-06 08:19:11 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,13 +18,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(size * nmemb);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
 
 
-	for (i = 0; i < (size * nmemb); i++)
+	for (i = 0; i < (nmemb * size); i++)
 		p[i] = '\0';
 
 	return (p);
