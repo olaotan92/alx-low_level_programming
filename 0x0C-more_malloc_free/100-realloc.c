a5c7a3fbc73b831e94c90054eee51b83acd50cc7
olaotan92 <elijahshawntel@gmail.com>
2022-10-06 22:16:51 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,38 +12,40 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, relloc;
+	char *ptr1;
+	char *old_ptr;
 	unsigned int i;
 
-	if (ptr != NULL)
-	{
-		clone = ptr;
-	}
-	else
-	{
-		return (malloc(new_size));
-	}
 	if (new_size == old_size)
-	{
 		return (ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
+
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
+		return (NULL);
+	}
 
-		return (0);
-	}
-	relloc = malloc(new_size);
+	if (!ptr)
+		return (malloc(new_size));
 
-	if (relloc == NULL)
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
 	{
-		return (0);
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
-	for (i = 0; i < (old_size || i < new_size); i++)
+
+	if (new_size > old_size)
 	{
-		*(relloc + i) = clone[i];
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
+
 	free(ptr);
-
-	return (relloc);
+	return (ptr1);
 }
