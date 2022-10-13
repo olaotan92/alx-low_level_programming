3a57b7fdb2cfc8d58bcfad01e3bf8ecf92a0da7b
olaotan92 <elijahshawntel@gmail.com>
2022-10-12 22:30:30 -0700

    main

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,8 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	char *opc = (char *) main;
+	int i, nbytes;
 
 	if (argc != 2)
 	{
@@ -19,24 +19,22 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	bytes = atoi(argv[1]);
+	nbytes = atoi(argv[1]);
 
-	if (bytes < 0)
+	if (nbytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
+	for (i = 0; i < nbytes; i++)
 	{
-		if (i == bytes - 1)
-		{
-			printf("02hhx\n", arr[i]);
-			break;
-		}
-		printf("02hhx ", arr[i]);
+		printf("%02x", opc[i] & 0xFF);
+
+		if (i != nbytes - 1)
+			printf(" ");
 	}
+
+	printf("\n");
 	return (0);
 }
