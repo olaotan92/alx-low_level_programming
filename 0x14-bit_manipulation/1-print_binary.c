cec70bfcd9e2cf4252220a13b280e2181fadd5bd
olaotan ojo <elijahshawntel@gmail.com>
2022-10-27 16:01:57 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: integer
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int on, i;
+	unsigned long int x;
+
+	on = 0;
+	for (i = 63; i >= 0; i--)
+	{
+		x = (n >> i) & 1;
+		if (x == 1)
+			on = 1;
+		if (on == 1)
+			_putchar(((n >> i) & 1) + '0');
+	}
+	if (n == 0)
+		_putchar('0');
+}
