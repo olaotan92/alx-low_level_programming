31b2b592da5c4d530edf8624348771046c21870f
olaotan ojo <elijahshawntel@gmail.com>
2022-10-31 07:32:25 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL);
+	if (text_content == NULL)
 	{
 		length = 0;
 		text_content = "";
