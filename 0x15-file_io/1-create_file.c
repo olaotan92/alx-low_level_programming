f18b10066747bc716991cb69c8a555f943fd1857
olaotan ojo <elijahshawntel@gmail.com>
2022-10-31 07:30:10 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
  * create_file - creates a file
  * @filename: name of file
  * @text_content: NULL terminated string to write to the file
@@ -9,29 +26,26 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
-	int rwr;
+	unsigned int length;
+	ssize_t bytes_written;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
+	if (text_content == NULL);
+	{
+		length = 0;
 		text_content = "";
+	}
+	else
+		length = _strlen(text_content);
 
-	for (nletters =0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	bytes_written = write(fd, text_content, length);
+	if (bytes_written == -1)
 		return (-1);
-
 	close(fd);
-
 	return (1);
 }
