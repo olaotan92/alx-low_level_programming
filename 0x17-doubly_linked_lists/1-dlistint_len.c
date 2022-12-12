9f5bc48df3063494420ba58ecde0c6a3af52f981
Olaotan Ojo <elijahshawntel@gmail.com>
2022-12-12 04:10:28 -0800

    print dlistint

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * dlistint_len - Counts the number of elements in a linked dlistint_t list.
- * @h: The head of the dlistint_t list.          
+ * @h: The head of the dlistint_t list.
  *
  * Return: The number of elements in the dlistint_t list.
  */
