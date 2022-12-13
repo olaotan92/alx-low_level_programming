73e851bc580747149cfb2b3e2c50297713b64f35
Olaotan Ojo <elijahshawntel@gmail.com>
2022-12-12 20:55:58 -0800

    dynamic

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings but use number of bytes inputted
+* @dest: string to be appended on
+* @src: string to be completed at the end of dest
+* @n: integer to be compared
+*
+* Return: new concatenated string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, count2 = 0;
+	while (*(dest + count) != '\0')
+	count++;
+	while (count2 < n)
+	{
+	*(dest + count) = *(src + count2);
+	if (*(src + count2) == '\0')
+	break;
+	count++;
+	count2++;
+	}
+	return (dest);
+}
--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -1,1 +1,1 @@
-This is about dynamic libraries
+This directory contains the main header file, libmy.a library as well as a bash script to execute the library file
